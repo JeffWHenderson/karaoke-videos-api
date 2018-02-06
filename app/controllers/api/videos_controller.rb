2eb248662d1b98c46d4726b062c7260caa121f75
JeffWHenderson <jeffrey.henderson17@gmail.com>
2018-02-06 17:26:26 -0500

    added a create action and video_params private method.

--- app/controllers/api/videos_controller.rb
+++ app/controllers/api/videos_controller.rb
@@ -3,8 +3,24 @@ class Api::VideosController < ApplicationController
     render json: Video.all
   end
 
+  def create
+    video = Video.new(video_params)
+
+    if video.save
+      render json: video
+    else
+      render json: { message: video.errors }, status: 400
+    end
+  end
+
   def show
     video = Video.find_by(id: params["id"])
     render json: video
   end
+
+  private
+  def video_params
+    params.require(:video).permit(:title, :artist, :youtube_url, :image_url)
+  end
+
 end
