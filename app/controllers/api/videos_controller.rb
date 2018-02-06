a102e6097b58ad121c1141e170840e0c73a00b7a
JeffWHenderson <jeffrey.henderson17@gmail.com>
2018-02-06 17:18:54 -0500

    added a show action to the video controller.

--- app/controllers/api/videos_controller.rb
+++ app/controllers/api/videos_controller.rb
@@ -2,4 +2,9 @@ class Api::VideosController < ApplicationController
   def index
     render json: Video.all
   end
+
+  def show
+    video = Video.find_by(id: params["id"])
+    render json: video
+  end
 end
