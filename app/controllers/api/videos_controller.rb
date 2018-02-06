8fc3e3f83aede1ca989a43cd01eeb08eaee5d6af
JeffWHenderson <jeffrey.henderson17@gmail.com>
2018-02-06 17:34:30 -0500

    added a destroy action in the videos controller.

--- app/controllers/api/videos_controller.rb
+++ app/controllers/api/videos_controller.rb
@@ -1,4 +1,6 @@
 class Api::VideosController < ApplicationController
+  before_action :set_video, only: [:show, :update, :destroy]
+
   def index
     render json: Video.all
   end
@@ -14,11 +16,30 @@ class Api::VideosController < ApplicationController
   end
 
   def show
-    video = Video.find_by(id: params["id"])
-    render json: video
+    render json: @video
+  end
+
+  def updated_at
+    if @video.update(video_params)
+      render json: @video
+    else
+      render json: { message: @video.errors }, status: 400
+    end
+  end
+
+  def destroy
+    if @video.destroy
+      render status: 204
+    else
+      render json: { message: "unable to delete this video" }, status: 400
+    end
   end
 
   private
+  def set_video
+    @video = Video.find_by(id: params["id"])
+  end
+
   def video_params
     params.require(:video).permit(:title, :artist, :youtube_url, :image_url)
   end
